6f04aa22ec2440c28d49edaad86f540920438da2
Łukasz Pisarczyk <lukasz.pisarczyk02@gmail.com>
2023-09-07 16:34:05 +0200

    Added ability to repair Pickaxe with Mending using exp gained from stone

--- src/main/java/me/xlucash/xldrop/utils/ExperienceManager.java
+++ src/main/java/me/xlucash/xldrop/utils/ExperienceManager.java
@@ -1,7 +1,9 @@
 package me.xlucash.xldrop.utils;
 
 import me.xlucash.xldrop.config.ConfigManager;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * Manages the experience points given to players in the Drop plugin.
@@ -22,6 +24,15 @@ public class ExperienceManager {
         int playerLevel = player.getLevel();
         double baseExpAmount = configManager.getDoubleForPath("stone_exp_drop");
         double adjustedExpAmount = baseExpAmount * (1 + (playerLevel * 0.1));
+
+        ItemStack itemInHand = player.getInventory().getItemInMainHand();
+        if (itemInHand.containsEnchantment(Enchantment.MENDING) && itemInHand.getDurability() > 0) {
+            // Repair pickaxe with certain amount of exp
+            double repairAmount = Math.min(adjustedExpAmount, itemInHand.getType().getMaxDurability() - itemInHand.getDurability()) / 2;
+            itemInHand.setDurability((short) (itemInHand.getDurability() - repairAmount));
+            adjustedExpAmount -= repairAmount;
+        }
+
         player.giveExp((int) adjustedExpAmount);
     }
 }
